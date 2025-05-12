8204ce2ef0ee21d90ba2bccbc51a1065e7fa09d5
Kenward Terhemba <codewithkenward@gmail.com>
2025-05-12 07:03:45 +0100

    add aggregate and window functions

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,12 @@
+SELECT users.users_id, users.first_name, COUNT(bookings.bookings_id) AS total_bookings
+FROM users
+LEFT JOIN bookings ON users.users_id = bookings.users_id
+GROUP BY users.users_id, users.first_name;
+
+
+SELECT properties.properties_id, properties.name, COUNT(bookings.bookings_id) AS total_bookings
+ROW_NUMBER() OVER ORDER BY COUNT(bookings.bookings_id) DESC AS rank
+FROM properties
+LEFT JOIN bookings ON properties.properties_id = bookings.properties_id
+GROUP BY properties.properties_id, properties.name
+ORDER BY rank;
