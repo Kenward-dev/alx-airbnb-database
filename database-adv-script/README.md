287a11ee26f8d1294a17787a67a082a5d18e6be4
Kenward Terhemba <codewithkenward@gmail.com>
2025-05-12 05:31:21 +0100

    add complec queries with joins

---
+++ database-adv-script/README.md
@@ -0,0 +1,4 @@
+# Advanced SQL Queries
+
+## Overview
+Created advanced SQL queries like INNER JOIN, LEFT JOIN, FULL OUTER JOIN, subqueries, aggregations and more.
---
+++ database-adv-script/joins_queries.sql
@@ -0,0 +1,13 @@
+SELECT users.first_name, bookings.booking_date
+FROM users
+INNER JOIN bookings ON users.user_id = bookings.user_id;
+
+
+SELECT properties.name, reviews.rating
+FROM properties
+LEFT JOIN reviews ON properties.property_id = reviews.property_id;
+
+
+SELECT users.first_name, bookings.booking_date
+FROM users
+FULL OUTER JOIN bookings ON users.user_id = bookings.user_id;
