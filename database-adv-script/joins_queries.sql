2955756e06298143df0d65c79d23d22352a44daa
Kenward Terhemba <codewithkenward@gmail.com>
2025-05-12 05:34:11 +0100

    add complecx queries with joins ordered in desc

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -5,7 +5,8 @@ INNER JOIN bookings ON users.user_id = bookings.user_id;
 
 SELECT properties.name, reviews.rating
 FROM properties
-LEFT JOIN reviews ON properties.property_id = reviews.property_id;
+LEFT JOIN reviews ON properties.property_id = reviews.property_id
+ORDER BY reviews.rating DESC;
 
 
 SELECT users.first_name, bookings.booking_date
